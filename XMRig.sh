5dad6d30c113e7f8efc778ba4cc95797f11f2874
fernandogdj <fernando.jr.dasilva@gmail.com>
2024-02-19 13:18:08 -0300

    Add files via upload

---
+++ XMRig.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./xmrig -o zeph.kryptex.network:7777 -u ZEPHYR2Pc2ULJmDvNgvCwRABwqYj9ZCFR8nR3tNUjx5x3tcY1DZgrFi4L9F4xJKyXvEkLfAVySDUQKHzcZFQiKzzbovtW1MyQ6R1A/js -k --coin zephyr -a rx/0 -t 2
